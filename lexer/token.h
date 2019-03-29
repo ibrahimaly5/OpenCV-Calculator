660ccd6447db1867af907b0cf9b2f6f76d2fef54
Ibrahim Aly <ialy@ualberta.ca>
2019-03-28 19:30:26 -0600

    added meh lexer

---
+++ lexer/token.h
@@ -0,0 +1,34 @@
+#ifndef _TOKEN_H
+#define _TOKEN_H
+
+using namespace std;
+
+enum class Token{
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    EQUAL_SIGN,
+    SUBTRACT_SIGN,
+    ADD_SIGN,
+    MULTIPLY_SIGN,
+    DIVIDE_SIGN,
+    DOT_SIGN,
+    NUM_ZERO,
+
+    NUM_ONE,
+    NUM_TWO,
+    NUM_THREE,
+    NUM_FOUR,
+    NUM_FIVE,
+    NUM_SIX,
+    NUM_SEVEN,
+    NUM_EIGHT,
+    NUM_NINE,
+
+
+    TRIG_SIN,
+    TRIG_COS,
+    TRIG_TAN
+};
+
+
+#endif
