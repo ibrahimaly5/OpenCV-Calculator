96fb53b98fc86b6d61cb51bce6238ed865e0f0e2
Raynard Dizon <43446567+rayd512@users.noreply.github.com>
2019-04-04 08:17:19 -0600

    Add files via upload

---
+++ Final/lexer.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "lexer.h"
+
+using namespace std;
+
+void lexer::LexInput(string input){
+  tokens.clear();
+
+  tokens.push_back( string(1,input[0]) );
+  string s;
+  for (int i=1; i<input.size(); i++){
+    s = string(1,input[i]);
+    if ( !s.compare("(") ){
+    tokens.push_back( "(" );
+    }else if ( !s.compare(")") ){
+      tokens.push_back( ")" );
+    }else if ( !s.compare("=") ){
+      tokens.push_back( "=" );
+    }else if ( !s.compare("-") ){
+      tokens.push_back( "-" );
+    }else if ( !s.compare("+") ){
+      tokens.push_back( "+" );
+    }else if ( !s.compare("X") || !s.compare("*") ){
+      tokens.push_back( "*" );
+    }else if ( !s.compare("÷") || !s.compare("/") ){
+      tokens.push_back( "/" );
+    }else if ( !s.compare(".") ){
+      tokens.back() = tokens.back() + s;
+    } else if (!s.compare("i")){
+      try {
+        stoi( tokens.back() );
+        tokens.back() = tokens.back() + s;
+      } catch(...) {
+        tokens.push_back(s);
+      }      
+    }
+    
+    else {
+      try {
+        stoi(s);
+        try {
+          stoi(tokens.back());
+          if (!tokens.back().find("i")){
+            tokens.push_back(s);
+          }else{
+            tokens.back() = tokens.back() + s;
+          }
+        } catch(...) {
+          tokens.push_back(s);
+        }
+
+      } catch(...) {}
+      
+    }
+  }
+}
+
+// void lexer::StringToToken(string s){
+  
+    
+//   // do this later too
+
+//   // else if ( !s.compare("sin") ){
+//   //     string second, third;
+//   // }
+// }
+
+vector<string> lexer::getTokens(){
+  return tokens;
+}
+
+// int main(){
+//   lexer trial1;
+
+//   string statement = "21(3*4)";
+//   trial1.LexInput(statement);
+
+//   vector<string> output;
+//   output = trial1.getTokens();
+//   for (int i=0; i<output.size(); i++){
+//     cout << output[i] << " ";
+//   }
+//   cout << endl;
+
+//   return 0;
+// }
